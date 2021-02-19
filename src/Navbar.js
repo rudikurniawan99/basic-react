fd7b8affd475b370e60ac53a217724d5225f9995
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-02-19 17:57:39 +0700

    add style with dynamic value via JSX

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,8 +7,13 @@ const Navbar = () => {
       <h1>My React Web</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/blog">Blog</a>
-        <a href="/about"></a>
+        <a href="/create" style={{ 
+          color: '#f7f7f7',
+          paddingLeft: '20px',
+          paddingRight: '20px',
+          backgroundColor: '#f1356d',
+          borderRadius: '5px'
+        }} >New Blog</a>
       </div>
     </nav>
   );
