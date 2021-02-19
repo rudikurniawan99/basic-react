e1a20b6a7a90b4897f7d3541e8bebcb00fe0d9e9
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-02-19 10:31:10 +0700

    learn dynamic template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+// everything you want to -import- from outside goes here
+import './App.css'
 
 function App() {
+  // Your -Logic- goes here
+  const student = {
+    name : 'Alan Faza',
+    age : 22
+  }
+  let name = 'Rudi Kurnaiwan'
+  const nameCheck = true
+  if(nameCheck){
+    name = 'Bambang Sudarmo'
+  }
+
+  const link = 'https://github.com'
+
+  // -endof logic
+
   return (
+    // Your -Template- that you want to display on screen goes here
     <div className="App">
-      <div className="bg-green-500">
-        <h2 className="bg-red-700">Ini dia outpukan</h2>   
-      </div>    
+      {/* rendering a value that we define before on the logic */}
+      <p>My name is <span className="font-bold"> {name} </span> </p>
+
+      {/* adding rendering an object only can be done by rendering a value */}
+      <p>My class mate is : <span> {student.name} </span> </p>
+
+      {/* Rendering element directly */}
+      <p> {'nama saya rudi Kurniawan'} </p>
+
+      {/* Url Link with dynamic value */}
+      <a target="_blank" href={link}>My Link</a>
+
     </div>
+    // -endof template
   );
 }
 
