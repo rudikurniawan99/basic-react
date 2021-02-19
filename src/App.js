84563e7b3b8d83ebbbbb6d1a1985470462935ce8
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-02-19 09:45:04 +0700

    add tailwindcss but still not working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 function App() {
   return (
     <div className="App">
-      
+      <div className="bg-green-500">
+        <h2 className="bg-red-700">Ini dia outpukan</h2>   
+      </div>    
     </div>
   );
 }
