830178339276db8c1d47597b30d4048f0129ccc0
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-02-20 09:42:06 +0700

    - using events - using hooks state to dynamic rendering on a value

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,20 @@
 import './Home.css'
+import { useState } from 'react'
+
 
 const Home = ()=>{
+  const [name, setName] = useState('Rudi Kurniawan')
 
   return (
     <div className="home">
-      This is home section
+      This is home section and my name is 
+      <span style={
+        {
+          fontWeight: 'bold'
+        }
+      }> {name} </span>
+      <br/>
+      <button onClick={()=> setName('Bambang Bojonegoro')} >Click Me</button>
     </div>
   )
 }
